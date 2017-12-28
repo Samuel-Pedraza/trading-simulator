0509f06eae0e35df1321cf8491ef03e0a48cb31f
samuel-pedraza <samdpedraza@gmail.com>
2017-12-27 18:41:38 -0800

    Added express

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Gdax = require('gdax');
 const publicClient = new Gdax.PublicClient();
 
-publicClient.getProductTicker('ETH-USD', function(error, response, data){
-	if(error){
-		throw error;
-	} else {
-		console.log(data['price']);
-	}
-});
+var http = require("http");
+
+http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type' : 'text/plain'})
+	response.end(
+	publicClient.getProductTicker('ETH-USD', function(error, response, data){
+		if(error){
+			throw error;
+		} else {
+			process.stdout.write(data['price']);
+		}
+	}));
+}).listen(8000, '127.0.0.1');
+console.log("Server Running on 127.0.0.1");
