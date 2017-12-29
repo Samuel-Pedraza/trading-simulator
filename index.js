a2eb305038d04b437608d6708171862b4b181262
samuel-pedraza <samdpedraza@gmail.com>
2017-12-28 16:49:01 -0800

    added nodemon, add view folder with index.html file

--- index.js
+++ index.js
@@ -2,32 +2,13 @@ const Gdax = require('gdax');
 const publicClient = new Gdax.PublicClient();
 const express = require('express');
 const app = express();
+const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 
 const MONGO_URL = 'mongodb://<user>:<password>@ds133597.mlab.com:33597/trading-simulator';
 
 app.get("/", function(req, res){
-	MongoClient.connect(MONGO_URL, (err, db) => {  
-		  if (err) {
-		    return console.log(err);
-		  }
-
-		  // Do something with db here, like inserting a record
-		  db.collection('notes').insertOne(
-		    {
-		      title: 'Hello MongoDB',
-		      text: 'Hopefully this works!'
-		    },
-		    function (err, res) {
-		      if (err) {
-		        db.close();
-		        return console.log(err);
-		      }
-		      // Success
-		      db.close();
-		    }
-		  )
-		});
+	res.sendFile(path.join(__dirname + "/views/index.html"))
 });
 
 app.listen(3000);
