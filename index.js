5e795cec46261cb481babb225ac8239aa451caa3
samuel-pedraza <samdpedraza@gmail.com>
2017-12-29 16:53:05 -0800

    update and push

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const MongoClient = require("mongodb").MongoClient;
 const MONGO_URL = 'mongodb://<user>:<password>@ds133597.mlab.com:33597/trading-simulator';
 
 app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname + "/views/index.html"))
+	publicClient.getProductHistoricRates('BTC-USD', { granularity: 3000 }, function(error, response, data){
+		console.log(data);
+	});
 });
 
 app.listen(3000);
